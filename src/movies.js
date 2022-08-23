b123e55b8a0e8315fe406439dab0e69879ff7c53
Gabi <gabi@mac-morales.local>
2022-08-23 23:33:26 +0200

    WEEK-4-lab-movies

--- src/movies.js
+++ src/movies.js
@@ -1,25 +1,55 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    let directors;
+    directors = moviesArray.map(
+        function (movie) {
+            return movie.director;
+        }
+    )
+    return directors;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    let stevenMovies;
+
+    stevenMovies = moviesArray.filter(
+        function (movie) {
+            // condicion 1: director tiene que ser Steven Spielberg
+            if (movie.director === 'Steven Spielberg') {
+                //cond 2: el genre tiene que contener el tipo Drama 
+                // ['Biography', 'Drama', 'History'].indexOf('Drama') = 
+                if (movie.genre.indexOf('Drama') !== -1) {
+                    return movie;
+                }
+            }
+        }
+    )
+    return stevenMovies.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) { 
+    
+    moviesArray.reduce(function(valorAnterior, valorActual){
+        return valorAnterior + valorActual;
+      });
+
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) { }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) { }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) { }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+function turnHoursToMinutes(moviesArray) { }
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function bestYearAvg(moviesArray) { }
